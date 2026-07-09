4b99c04a39d82a3b79ba8e97a621a97f824a3ad6
Adam Sabet <136860375+Odysseyc@users.noreply.github.com>
2026-07-09 19:37:00 -0400

    Implement joint angle interpolation function
    Added a function to linearly interpolate joint angles between two configurations over a specified number of steps.

---
+++ control/trajectory.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def interpolate_joints(start_angles, end_angles, steps=100):
+    """
+    Linearly interpolate between two joint configurations.
+
+    Parameters:
+        start_angles: starting joint angles (radians)
+        end_angles: target joint angles (radians)
+        steps: number of intermediate positions
+
+    Returns:
+        Array of joint configurations
+    """
+
+    start_angles = np.array(start_angles)
+    end_angles = np.array(end_angles)
+
+    trajectory = []
+
+    for alpha in np.linspace(0, 1, steps):
+        angles = (1-alpha)*start_angles + alpha*end_angles
+        trajectory.append(angles)
+
+    return np.array(trajectory)
+
+
+if __name__ == "__main__":
+
+    start = np.array([0, 0, 0])
+    end = np.array([1, 0.5, -1])
+
+    path = interpolate_joints(start, end)
+
+    print(path.shape)
